f23d7e0885291c06e88d4ecd5ac8e9de03d9bc60
Thomas Stephens <tmsteph1290@gmail.com>
2025-05-17 19:54:57 -0700

    Update vite.config.js to try and get things working

--- the/vite.config.js
+++ the/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    server: {
-        port: 8764,
-        open: '/examples/toy.html'
-    },
-  })
+  server: {
+    port: 8764,
+    open: '/examples/toy.html',
+    headers: {
+      'Content-Security-Policy': "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;"
+    }
+  }
+})
